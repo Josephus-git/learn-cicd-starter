5d16f647bd7e3064da6d17fb7dbf28d428bbd5bc
Josephus <otejerejosephus@gmail.com>
2025-06-30 19:30:14 +0100

    Revert "add fmt tests"

--- json.go
+++ json.go
@@ -27,10 +27,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
 		w.WriteHeader(500)
+		return
 	}
 	w.WriteHeader(code)
-	_, err = w.Write(dat)
-	if err != nil {
-		log.Printf("error writing data")
-	}
+	w.Write(dat)
 }
